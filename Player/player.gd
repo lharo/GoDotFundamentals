3b507c81d27e3a4276700d6a1547006fe8d4ab8c
lharo <esteban6000@gmail.com>
2025-05-24 19:40:24 -0600

    Handle Delta and Movement

--- Player/player.gd
+++ Player/player.gd
@@ -1,31 +1,26 @@
 extends CharacterBody2D
 
+const MAX_SPEED = 1000
+const ACCELERATION = 100;
+const FRICTION = 200;
+
 func _ready() -> void:
 	print("Hello World")
 
-#Override base class, ran every frame. Delta is how long last frame took
-func _physics_process(delta: float) -> void:
-#	This is not the best approach using input vector
-	#if Input.is_action_pressed("ui_right"):
-##		#position.x += 10; Do not do this use vector
-		#velocity.x = 4;
-	#elif Input.is_action_pressed("ui_left"):
-		#velocity.x = -4;
-	#elif Input.is_action_pressed("ui_up"):
-		#velocity.y = -4;
-	#elif Input.is_action_pressed("ui_down"):
-		#velocity.y = 4;
-	#else:
-		#velocity.x = 0
-		#velocity.y = 0
-	
+func _physics_process(delta: float) -> void:	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
-	
+	input_vector = input_vector.normalized()
+#	CURRENTLY WE RUN QUICKER ON DIAGONALS BECAUSE OF PITHAGOREAN THEOREM
+#	We can Normalize the vector this means creating a circle that has same space to every corner 
+#	will clip to same lenght as x or y	
+
+#	ACCELERATE SO DO NOT REACH TOP SPEED IMMEDIATLY USE DELTA TO NOT TIE OURSELVES TO FRAME RATE
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * ACCELERATION * delta # Modifies CharacterBody2D.velocity
+		velocity = velocity.limit_length(MAX_SPEED)    # Modifies CharacterBody2D.velocity
+#	DECELERATE SAME AS ABOVE
 	else:
-		velocity = Vector2.ZERO
-	
-	move_and_collide(velocity)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) # Modifies CharacterBody2D.velocity	
+	move_and_collide(velocity * delta )
